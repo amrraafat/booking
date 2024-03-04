5aef3a74000c26856b931f01a3020e864811c15c
Mango <Mango@DESKTOP-0JMDECM>
2024-03-04 15:27:30 +0200

    Add project files.

---
+++ Infarstuructre/Domin/BookingDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infarstuructre.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infarstuructre.Domin
+{
+    public class BookingDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public BookingDbContext(DbContextOptions<BookingDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
